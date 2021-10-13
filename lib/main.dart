acf25a3d997a5c0ab040ac5a5b3471ad17a28c98
William Davies <william@withutraining.com>
2021-10-14 01:17:14 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:nightscreamers/screens/pulse_screen.dart';
-import 'package:nightscreamers/screens/slogan_screen.dart';
-// import 'package:nightscreamers/screens/home_screen_pulse.dart';
+
+import 'ui/router/router.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final AppRouter _appRouter = AppRouter();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'NightScreamers',
-      initialRoute: SloganScreen.id,
-      debugShowCheckedModeBanner: false,
-      routes: {
-        SloganScreen.id: (context) => SloganScreen(),
-        PulseScreen.id: (context) => PulseScreen(),
-      },
+      title: 'Nightscreamers',
+      onGenerateRoute: _appRouter.onGenerateRoute,
     );
   }
 }
