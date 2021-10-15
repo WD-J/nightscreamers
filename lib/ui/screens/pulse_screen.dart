6216875f1966388dfd29a441a27009a3f7517c2f
william <william.davies@fygi.no>
2021-10-16 00:13:07 +0200

    refactor

--- lib/ui/screens/pulse_screen.dart
+++ lib/ui/screens/pulse_screen.dart
@@ -1,8 +1,5 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
-import 'package:video_player/video_player.dart';
 
 class PulseScreen extends StatefulWidget {
   static const String id = 'pulse_screen';
@@ -21,36 +18,9 @@ class _PulseScreenState extends State<PulseScreen>
   late AnimationController _opacityAnimationController;
   late Animation<double> _opacityAnimationTween;
 
-  late String a;
-
-  bool videoActivated = false;
-
-  late VideoPlayerController _entryVideo;
-  late ChewieController _chewieController;
-
-  Future<void> initializeVideoPlayer() async {
-    // TODO: Replace with VideoPlayerController.asset if necessary / efficient
-    _entryVideo = VideoPlayerController.network(
-        'https://dw.convertfiles.com/files/0519485001611875423/nightscreamers.264');
-
-    await _entryVideo.initialize();
-
-    _chewieController = ChewieController(
-      videoPlayerController: _entryVideo,
-      autoPlay: false,
-      looping: true,
-      showControls: false,
-      startAt: Duration(milliseconds: 2200),
-    );
-
-    _chewieController.setVolume(0.5);
-  }
-
   @override
   void initState() {
     super.initState();
-
-    initializeVideoPlayer();
 
     timeDilation = timeDilation + 2.5;
 
@@ -60,7 +30,7 @@ class _PulseScreenState extends State<PulseScreen>
       vsync: this,
     );
 
-    _animationTween = Tween(begin: 0.0, end: -75.0).animate(CurvedAnimation(
+    _animationTween = Tween(begin: 50.0, end: 0.0).animate(CurvedAnimation(
         parent: _animationController, curve: Curves.easeInOutQuint));
 
     _animationController.addListener(() => setState(() {}));
@@ -79,95 +49,45 @@ class _PulseScreenState extends State<PulseScreen>
       setState(() {});
     });
     _opacityAnimationController.forward();
-    // _opacityAnimationController.repeat(reverse: true, max: 1);
-  }
-
-  @override
-  void dispose() {
-    _entryVideo.dispose();
-    _chewieController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: !videoActivated
-          ? Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.red,
-                      ),
-                      BoxShadow(
-                        color: Colors.black,
-                        spreadRadius: -_animationTween.value,
-                        blurRadius: 100.0,
-                      ),
-                    ],
-                  ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.red,
                 ),
-                Column(
-                  children: [
-                    Expanded(child: Container()),
-                    Center(
-                      child: Container(
-                        padding: EdgeInsets.only(left: 50.0, right: 50.0),
-                        child: FittedBox(
-                          fit: BoxFit.scaleDown,
-                          child: Opacity(
-                            opacity: _opacityAnimationTween.value,
-                            child: InkWell(
-                              onTap: () {
-                                setState(() {
-                                  videoActivated = true;
-                                  _chewieController.play();
-                                });
-                              },
-                              focusColor: Colors.transparent,
-                              splashColor: Colors.transparent,
-                              hoverColor: Colors.transparent,
-                              highlightColor: Colors.transparent,
-                              child: AutoSizeText(
-                                "nightscreamers",
-                                maxLines: 1,
-                                style: TextStyle(
-                                  fontFamily: 'Nightscreamers',
-                                  fontSize: 275,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    // Spacer(flex: 2),
-                    Expanded(child: SizedBox()),
-                    Expanded(child: SizedBox()),
-                  ],
+                BoxShadow(
+                  color: Colors.black,
+                  spreadRadius: -_animationTween.value,
+                  blurRadius: 100.0,
                 ),
               ],
-            )
-          : Center(
-              child: _chewieController.videoPlayerController.value != null &&
-                      _chewieController
-                          .videoPlayerController.value.isInitialized
-                  ? Chewie(
-                      controller: _chewieController,
-                    )
-                  : Container(
-                      child: Text(
-                        "LOADING",
-                        style: TextStyle(
-                            color: Colors.white.withAlpha(
-                                _opacityAnimationTween.value.toInt())),
-                      ),
-                    ),
             ),
+          ),
+          Center(
+            child: Opacity(
+              opacity: _opacityAnimationTween.value,
+              child: FittedBox(
+                child: Text(
+                  "nightscreamers",
+                  style: TextStyle(
+                    fontFamily: 'Nightscreamers',
+                    fontSize: 200,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
